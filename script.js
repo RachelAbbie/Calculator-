82bfed62aaa5d45754f8384febe62813fb242dbd
RachelAbbie <rachelabbief@gmail.com>
2020-12-18 12:50:44 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+    
+    chooseOperator(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const alLClearButtons = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
